52dfabd57046aaf76e1dbdef12bbe7b4953298ce
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-11 19:33:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
+  array.find do |item|
+    cheese_types.include?(item)
 
 end
